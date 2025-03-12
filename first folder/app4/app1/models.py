ef28f78b089246c74e412c94b780e311046042c9
pavan-lp11 <pavana59340@gmail.com>
2025-03-12 17:52:06 +0530

    customer_management and gps mangement added

--- first folder/app4/app1/models.py
+++ first folder/app4/app1/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.    
 class customer_master(models.Model):
-    customer_id=models.CharField(max_length=20)
+    customer_id=models.CharField(max_length=20,unique=True)
     customer_name=models.CharField(max_length=30)
     phone=models.IntegerField()
     status=models.IntegerField(default=1)
@@ -15,8 +15,8 @@ class customer_details(models.Model):
     address=models.CharField(max_length=30)
     status=models.IntegerField(default=1)
 
-class add_gps(models.Model):
-    gps_id=models.CharField(max_length=50)
+class gps_master(models.Model):
+    gps_id=models.CharField(max_length=50,unique=True)
     customer_name=models.CharField(max_length=40)
     imei_no=models.IntegerField()
     vehicle_registration_no=models.CharField(max_length=20)
@@ -25,49 +25,17 @@ class add_gps(models.Model):
     vehicle_type=models.CharField( max_length=50)
     status=models.IntegerField(default=1)
 
-# class gps_details(models.Model):
-
-
-class add_enquiry(models.Model):
-    customer_id=models.CharField( max_length=50)
-    name=models.CharField( max_length=50)
-    phone=models.CharField( null=True,max_length=50)
-    registration_no=models.CharField( max_length=50)
+class gps_details(models.Model):
+    gps_id=models.CharField(max_length=50)
+    customer_name=models.CharField(max_length=40)
+    imei_no=models.IntegerField()
+    vehicle_registration_no=models.CharField(max_length=20)
     vehicle_model=models.DateField( auto_now=False, auto_now_add=False)
-    manufacturing_year=models.DateField( auto_now=False, auto_now_add=False)
-    previous_policy_no=models.IntegerField()
-    insurance_name=models.CharField( max_length=50)
-    expiry_date=models.DateField( auto_now=False, auto_now_add=False)
+    vehicle_make=models.DateField( auto_now=False, auto_now_add=False)
     vehicle_type=models.CharField( max_length=50)
-    start_date=models.DateField( auto_now=False, auto_now_add=False)
-    end_date=models.DateField( auto_now=False, auto_now_add=False)
-
-class add_insurance(models.Model):
-    customer_id=models.CharField( max_length=60)
-    name=models.CharField( max_length=50)
-    phone=models.CharField( null=True,max_length=50)
-    registration_no=models.CharField( max_length=50)
-    vehicle_model=models.DateField( auto_now=False, auto_now_add=False)
-    year=models.DateField( auto_now=False, auto_now_add=False,null=True)
-    policy_no=models.IntegerField()
-    insurance_name=models.CharField( max_length=50)
-    expiry_date=models.DateField( auto_now=False, auto_now_add=False)
-    vehicle_type=models.CharField( max_length=50)
-    start_date=models.DateField( auto_now=False, auto_now_add=False)
-    end_date=models.DateField( auto_now=False, auto_now_add=False)
-
-    
-
-
-
-
-    
-
-
-
-
-
-
-
-
+    installation_date=models.DateField(auto_now=False, auto_now_add=False)
+    reminder_date=models.DateField(auto_now=False, auto_now_add=False)
+    next_reminder_date=models.DateField(auto_now=False, auto_now_add=False)
+    plan=models.CharField(max_length=40)
+    status=models.IntegerField(default=1)
 
--- first folder/app4/app1/views.py
+++ first folder/app4/app1/views.py
@@ -1,8 +1,7 @@
 from django.shortcuts import render,redirect
 from django.contrib.auth.decorators import login_required,user_passes_test
-from app1.models import customer_master,customer_details,add_enquiry,add_gps,add_insurance
+from app1.models import customer_master,customer_details,gps_master,gps_details
 from django.contrib.auth.models import User
-from django.contrib.auth import login,authenticate,logout
 
 
 # Create your views here.
@@ -31,101 +30,37 @@ def list_customer(request):
         data=customer_details.objects.filter(status=1)
         context={'sent':data}
         return render(request,'list_customer.html',context)
-@login_required
-def add_enquiries(request):
-        if request.method =='POST':
-                name=request.POST['name']
-                customer=request.POST['customer id']
-                phone=request.POST['phone']
-                registration_no=request.POST['registration no']
-                vehicle_model=request.POST['vehicle model']
-                manufacturing_year=request.POST['manufacturing year']
-                previous_policy_no=request.POST['previous policy no']
-                insurance_name=request.POST['insurance name']
-                vehicle_type=request.POST['vehicle type']
-                expiry_date=request.POST['expiry date']
-                start_date=request.POST['star date']
-                end_date=request.POST['end_date']
-                add_enquiry.objects.create(name=name,customer_id=customer,phone=phone,registration_no=registration_no,vehicle_model=vehicle_model,manufacturing_year=manufacturing_year,previous_policy_no=previous_policy_no,insurance_name=insurance_name,vehicle_type=vehicle_type,expiry_date=expiry_date,start_date=start_date,end_date=end_date)
 
-                return render(request,'add_enquiry.html')
-        return render(request,'add_enquiry.html')
-def list_enquiry(request):
-        value=add_enquiry.objects.all()
-        context={'sent':value}
-        return render(request,'list_enquiry.html',context)
 @login_required
 def add_gps_management(request):
         if request.method =='POST':
-                gps_id=request.POST['gps_id']
-                customer_name=request.POST['customer_name']
+                gps_id=request.POST['gps_id']              
                 imei_no=request.POST['imei_no']
                 vehicle_registration_no=request.POST['vehicle_registration_no']
                 vehicle_model=request.POST['vehicle_model']
                 vehicle_make=request.POST['vehicle_make']
                 vehicle_type=request.POST['vehicle_type']
-                add_gps.objects.create(gps_id=gps_id,customer_name=customer_name,imei_no=imei_no,vehicle_registration_no=vehicle_registration_no,vehicle_model=vehicle_model,vehicle_make=vehicle_make,vehicle_type=vehicle_type)
+                installation_date=request.POST['installation_date']
+                reminder_date=request.POST['reminder_date']
+                next_reminder_date=request.POST['next_reminder_date']
+                plan=request.POST['plan']
+                gps_master.objects.create(gps_id=gps_id,customer_name='ii',imei_no=imei_no,vehicle_registration_no=vehicle_registration_no,vehicle_model=vehicle_model,vehicle_make=vehicle_make,vehicle_type=vehicle_type)
+                gps_details.objects.create(gps_id=gps_id,customer_name='ii',imei_no=imei_no,vehicle_registration_no=vehicle_registration_no,vehicle_model=vehicle_model,vehicle_make=vehicle_make,vehicle_type=vehicle_type,installation_date=installation_date,reminder_date=reminder_date,next_reminder_date=next_reminder_date,plan=plan)
                 return redirect('list_gps') 
-        orderby_id=add_gps.objects.order_by('-gps_id').first()
+        orderby_id=gps_details.objects.order_by('-gps_id').first()
         if orderby_id:
                 old_id=int(orderby_id.gps_id[3:])
                 fresh_id=f"GPS{old_id+1:04d}"
                 print(fresh_id)
         else:
-                fresh_id="GPS0001"
-        context={'fresh_id':fresh_id}
+             fresh_id="GPS0001"
+        data=customer_details.objects.all()
+        context={'fresh_id':fresh_id,'sent':data}
         return render(request,"add_gps.html",context)
 
 def list_gps_management(request):
-        content=add_gps.objects.filter(status=1)
+        content=gps_master.objects.filter(status=1)
         context={'sent':content}
         return render(request,'list_gps.html',context)
- 
-def add_sign_class(request):
-        if request.method =='POST':
-                username=request.POST['username']
-                password=request.POST['password']
-                user=authenticate(username=username,password=password) 
-                if user is not None:
-                        login(request,user)
-                        return redirect('add_gps')
-                else:
-                        return render(request,'class.html')
-        return render(request,'class.html')
-
-def sign_out(request):
-        logout(request)
-        return redirect('class')
-
-def city_autocomplete(request):
-    data = customer_master.objects.all()
-    context = {'sent':data}
-    # Add_Insurance;.objects.create(invoice_number = 1, customer_name = "Test", date = "25-10-2001", total_amount = 1000)
-
-    return render(request,'render.html', context) 
-
-def add_insurancesss(request):
-        if request.method =='POST':
-                name=request.POST['name']
-                customer_id=request.POST['customer_id']
-                phone=request.POST['phone']
-                registration_no = request.POST['registration_no']
-                vehicle_model=request.POST['vehicle_model']
-                year=request.POST['year']
-                policy_no=request.POST['policy_no']
-                insurance_name=request.POST['insurance_name']
-                vehicle_type=request.POST['vehicle_type']
-                expiry_date = request.POST['expiry_date']
-                start_date=request.POST['start_date']
-                end_date=request.POST['end_date'] 
-                daa = add_insurance.objects.create(name=name,customer_id=customer_id,phone=phone,registration_no=registration_no,vehicle_model=vehicle_model,year=year,policy_no=policy_no,insurance_name=insurance_name,vehicle_type=vehicle_type,expiry_date=expiry_date,start_date=start_date,end_date=end_date)
-        
-        data11=customer_master.objects.all()
-        context={'data':data11}
-        return render(request,'add_insurance.html',context)
 
 
-def list_insurance(request):
-        part=add_insurance.objects.all()
-        concept={'sent':part}
-        return render(request,'list_insurance.html',concept)
